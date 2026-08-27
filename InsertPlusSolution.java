0dab553ed3c520ddc02f4aa2bb105b59ad991ef7
Kevin Tang <114013311+TanVek@users.noreply.github.com>
2026-08-27 18:30:45 -0400

    Add files via upload

---
+++ InsertPlusSolution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.*;
+public class InsertPlusSolution {
+      public static void main(String[] args) {
+         ArrayList<String> list = new ArrayList<>(
+            List.of("one", "two", "three", "four") );
+         System.out.println("list = " + list);
+
+         // insert a + in front of every list element
+         
+         for (int i = 1; i < list.size(); i+=2)//.... YOUR CODE HERE ....
+            list.add(i, "+");
+         System.out.println("updated list = " + list);
+    }
+}
